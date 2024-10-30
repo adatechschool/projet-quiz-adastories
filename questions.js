134632c52a26ed8b937e4ac037546eebe36deec7
IkuroMaira <gwen.bussac@gmail.com>
2024-10-30 15:06:35 +0100

    Add js file

---
+++ questions.js
@@ -0,0 +1,45 @@
+const quiz_adastories = {
+    questions: [
+        {
+            text: "Tu peux voir mon père dans les Visiteurs.",
+            options: [
+                "Gwenaëlle",
+                "Laura",
+                "Jude",
+                "Hugo"
+            ],
+            correct_answer: "Laura"
+        },
+        {
+            text: "J’ai un piercing à la langue.",
+            options: [
+                "Marie",
+                "Leyane",
+                "Lauriane",
+                "Daniel"
+            ],
+            correct_answer: "Marie"
+        },
+        {
+            text: "Je me suis fait.e virer parce que je me suis disputé.e avec un client. Alors que j’avais signé un contrat pour gagner 7k en 3 mois.",
+            options: [
+                "Ludovic",
+                "Hugo",
+                "Leyane",
+                "Raïssa"
+            ],
+            correct_answer: "Leyane"
+        },
+        {
+            text: "Pour retrouver mon date dans un lieu précis (avant d'aller dans un bar). On a organisé un jeu de piste avec des énigmes.",
+            options: [
+                "Majda",
+                "Stella",
+                "Ange",
+                "Gwenaëlle"
+            ],
+            correct_answer: "Gwenaëlle"
+        },
+        // Autres questions...
+    ]
+};
