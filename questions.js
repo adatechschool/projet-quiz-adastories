81772b074ff9296cce4449668d59fa4fe509fb0b
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2024-10-30 12:20:48 +0100

    update css + add js

---
+++ questions.js
@@ -0,0 +1,45 @@
+export const anecdotes = {
+    questions: [
+    {
+        text: "On peut voir mon père dans les visiteurs",
+        options: [
+          "Laura",
+          "Hugo",
+          "Stella",
+          "Gwenaëlle"
+        ],
+        correct_answer: "Laura"
+    },
+    {
+        text: "J'ai un piercing à la langue",
+        options: [
+          "Daniel",
+          "Leyane",
+          "Marie",
+          "Lauriane"
+        ],
+        correct_answer: "Marie"
+    },
+    {
+        text: "Je me suis fait virer de mon job après un malentendu avec un client alors que j'avais signé un gros contrat à 7000€",
+        options: [
+          "Raïssa",
+          "Leyane",
+          "Ange",
+          "Léandre"
+        ],
+        correct_answer: "Leyane"
+    }
+
+    {
+        text: "Anecdote numéro 4 à ajouter plus tard",
+        options: [
+          "Prénom 1",
+          "Prénom 2",
+          "Prénom 3",
+          "Prénom 4"
+        ],
+        correct_answer: "Prénom 2"
+    }
+    ]
+  };
