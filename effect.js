f5686724f83bc519d112c6681de5d924a6fe69d3
IkuroMaira <gwen.bussac@gmail.com>
2024-11-01 10:58:00 +0100

    Add effect mousehover

---
+++ effect.js
@@ -0,0 +1,29 @@
+document.querySelector('.options').onmousemove = (e) => {
+
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    e.target.style.setProperty('--x', `${ x }px`)
+    e.target.style.setProperty('--y', `${ y }px`)
+
+}
+
+document.querySelector('#next-button').onmousemove = (e) => {
+
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    e.target.style.setProperty('--x', `${ x }px`)
+    e.target.style.setProperty('--y', `${ y }px`)
+
+}
+
+document.querySelector('#replay-button').onmousemove = (e) => {
+
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    e.target.style.setProperty('--x', `${ x }px`)
+    e.target.style.setProperty('--y', `${ y }px`)
+
+}
--- game.js
+++ game.js
@@ -1,23 +1,72 @@
 import { quiz_adastories } from './questions.js'; // Import des questions
 
+// Pour suivre a question actuelle
+let currentQuestionIndex = 0;
+
 // DOM affichage
-const getQuestion = document.querySelector('.question');
+const question = document.querySelector('.question');
 // console.log(getQuestion);
-const getOptions = document.querySelector('.options');
+const options = document.querySelector('.options');
 // console.log(getOptions);
-const getNextBtn = document.querySelector('#next-button');
+const nextBtn = document.querySelector('#next-button');
 // console.log(getNextBtn);
+const replayBtn = document.querySelector('#replay-button');
 
-let firstQuestion = quiz_adastories.questions[0];
-// console.log(firstQuestion);
+function loadQuestion() {
 
-// injection du texte dans le DOM :
-getQuestion.innerText = firstQuestion.text;
+    // Vider le conteneur des options
+    options.innerHTML = '';
 
-for (const option of firstQuestion.options) {
-    const addBtnOption = document.createElement("button");
-    
-    getOptions.appendChild(addBtnOption);
-    addBtnOption.classList.add('button-options') ;
-    addBtnOption.innerText = option;   
+    // Récupère la question actuelle
+    let currentQuestion = quiz_adastories.questions[currentQuestionIndex];
+    // console.log("index: ", currentQuestionIndex);
+    // console.log('question: ', currentQuestion);
+
+    // injection du texte dans le DOM :
+    question.innerText = currentQuestion.text;
+
+    for (const option of currentQuestion.options) {
+        const addBtnOption = document.createElement("button");
+
+        options.appendChild(addBtnOption);
+        addBtnOption.classList.add('button-options') ;
+        addBtnOption.innerText = option;
+    }
 }
+
+nextBtn.addEventListener('click', () => {
+
+    // Incrémenter l'index de la question
+    currentQuestionIndex++;
+
+    // Vérifier s'il reste des questions
+    if (currentQuestionIndex < quiz_adastories.questions.length) {
+        // console.log("On n'est pas à la fin");
+        // Afficher la question suivante
+        loadQuestion();
+    } else {
+        // console.log("Il n'y a plus de question");
+        // Si plus de questions, indiquer la fin du quiz
+        question.innerText = 'Le quiz est terminé !';
+        options.innerHTML = ''; // Effacer les options
+        nextBtn.style.display = 'none'; // Cacher le bouton Suivant
+        replayBtn.style.display = 'inline-block'; // Afficher le bouton Rejouer
+    }
+})
+
+// Fonction pour réinitialiser le quiz
+replayBtn.addEventListener('click', () => {
+    // Réinitialiser l'index
+    currentQuestionIndex = 0;
+    // Cacher le bouton Rejouer et afficher le bouton Suivant
+    replayBtn.style.display = 'none';
+    nextBtn.style.display = 'inline-block';
+    // Recharger la première question
+    loadQuestion()
+});
+
+function checkAnswer() {
+
+}
+
+loadQuestion()
