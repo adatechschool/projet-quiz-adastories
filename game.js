9b4e8bf2b1a47e30575cb6cc06c8b801b4542cce
IkuroMaira <gwen.bussac@gmail.com>
2024-11-04 10:51:11 +0100

    Start loadQuestion function

--- game.js
+++ game.js
@@ -2,21 +2,38 @@ import { quiz_adastories } from './questions.js'; // Import des questions
 
 // DOM affichage
 const getQuestion = document.querySelector('.question');
-// console.log(getQuestion);
 const getOptions = document.querySelector('.options');
-// console.log(getOptions);
 const getNextBtn = document.querySelector('#next-button');
-// console.log(getNextBtn);
 
-let firstQuestion = quiz_adastories.questions[3];
+let currentQuestionIndex = 0; // Qd tu cliques ça fait + 1
+
+let firstQuestion = quiz_adastories.questions[3]; // faut que ça bouge
 // console.log(firstQuestion);0
-// injection du texte dans le DOM :
-getQuestion.innerText = firstQuestion.text;
 
-for (const option of firstQuestion.options) {
-    const addBtnOption = document.createElement("button");
+// Ici on affiche les options
+// for (const option of firstQuestion.options) {
+//     const addBtnOption = document.createElement("button");
+//
+//     getOptions.appendChild(addBtnOption);
+//     addBtnOption.classList.add('button-options') ;
+//     addBtnOption.innerText = option;
+// }
 
-    getOptions.appendChild(addBtnOption);
-    addBtnOption.classList.add('button-options') ;
-    addBtnOption.innerText = option;
+function loadQuestion() {
+    let currentQuestion = quiz_adastories.questions[currentQuestionIndex];
+    console.log(currentQuestion);
+
+    // injection du texte dans le DOM :
+    getQuestion.innerText = currentQuestion.text;
+
+    // ici c'est les options
+    for (const option of firstQuestion.options) {
+        const addBtnOption = document.createElement("button");
+
+        getOptions.appendChild(addBtnOption);
+        addBtnOption.classList.add('button-options') ;
+        addBtnOption.innerText = option;
+    }
 }
+
+loadQuestion();
