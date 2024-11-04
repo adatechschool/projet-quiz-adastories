a94ce51199e245be8fd032ee5d2bba7a9fdb0967
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2024-11-04 15:13:39 +0100

    update js

--- game.js
+++ game.js
@@ -2,22 +2,67 @@ import { quiz_adastories } from './questions.js'; // Import des questions
 
 // DOM affichage
 const getQuestion = document.querySelector('.question');
-// console.log(getQuestion);
 const getOptions = document.querySelector('.options');
-// console.log(getOptions);
 const getNextBtn = document.querySelector('#next-button');
-// console.log(getNextBtn);
+const getReplayBtn = document.querySelector('#replay-button')
 
-let firstQuestion = quiz_adastories.questions[0];
-// console.log(firstQuestion);
+let currentQuestionIndex = 0; // Qd tu cliques ça fait + 1
 
-// injection du texte dans le DOM :
-getQuestion.innerText = firstQuestion.text;
+let firstQuestion = quiz_adastories.questions[3]; // faut que ça bouge
+// console.log(firstQuestion);0
 
-for (const option of firstQuestion.options) {
-    const addBtnOption = document.createElement("button");
-    
-    getOptions.appendChild(addBtnOption);
-    addBtnOption.classList.add('button-options') ;
-    addBtnOption.innerText = option;   
+// Ici on affiche les options
+// for (const option of firstQuestion.options) {
+//     const addBtnOption = document.createElement("button");
+//
+//     getOptions.appendChild(addBtnOption);
+//     addBtnOption.classList.add('button-options') ;
+//     addBtnOption.innerText = option;
+// }
+
+function loadQuestion() {
+    let currentQuestion = quiz_adastories.questions[currentQuestionIndex];
+    console.log(currentQuestion);
+
+    getOptions.innerText =""
+    // injection du texte dans le DOM :
+    getQuestion.innerText = currentQuestion.text;
+
+    // ici c'est les options
+    for (const option of currentQuestion.options) {
+        const addBtnOption = document.createElement("button");
+
+        getOptions.appendChild(addBtnOption);
+        addBtnOption.classList.add('button-options') ;
+        addBtnOption.innerText = option;
+    }
 }
+
+getNextBtn.addEventListener('click', () => {
+    // Incrémenter l'index de la question
+    currentQuestionIndex++;
+    //console.log(currentQuestionIndex)
+
+     // Vérifier s'il reste des questions
+  if (currentQuestionIndex < quiz_adastories.questions.length) {
+    // Afficher la question suivante
+    loadQuestion();
+  } else {
+   // Si plus de questions, indiquer la fin du quiz
+    getQuestion.innerText = 'le quizz est fini';
+    getOptions.innerHTML = ''; // Effacer les options
+    getNextBtn.style.display = 'none'; // Cacher le bouton Suivant
+    getReplayBtn.style.display = 'inline-block'; // Afficher le bouton replay
+  }
+})
+
+loadQuestion();
+
+getReplayBtn.addEventListener('click', () => {
+    currentQuestionIndex = 0
+    getReplayBtn.style.display = 'none'
+    getNextBtn.style.display = 'inline-block';
+     loadQuestion()
+})
+
+
