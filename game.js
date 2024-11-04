c9f22f5aed1c8c0bd30b572dd927bef479e7dc17
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2024-11-04 16:57:34 +0100

    clean js

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ const getReplayBtn = document.querySelector('#replay-button')
 
 let currentQuestionIndex = 0; // Qd tu cliques ça fait + 1
 
-let firstQuestion = quiz_adastories.questions[3]; // faut que ça bouge
+let firstQuestion = quiz_adastories.questions[0]; // faut que ça bouge
 // console.log(firstQuestion);0
 
 // Ici on affiche les options
@@ -22,7 +22,6 @@ let firstQuestion = quiz_adastories.questions[3]; // faut que ça bouge
 
 function loadQuestion() {
     let currentQuestion = quiz_adastories.questions[currentQuestionIndex];
-    console.log(currentQuestion);
 
     getOptions.innerText =""
     // injection du texte dans le DOM :
@@ -37,15 +36,11 @@ function loadQuestion() {
         addBtnOption.innerText = option;
 
         addBtnOption.addEventListener('click', () => {
-          // console.log(option)
           const optionBtn = document.querySelectorAll('.button-options')
-          console.log("optionBtn : ", optionBtn)
 
           for(const button of optionBtn){
-            button.classList.remove('untrue')} // rendre les autres boutons non cliquables
-
-        console.log("value: ", option)
-        console.log("correct :", currentQuestion.correct_answer)
+            button.classList.remove('untrue')
+          } // il faut rendre les autres boutons non cliquables
 
           if (option === currentQuestion.correct_answer) {
             addBtnOption.classList.add('correct')
@@ -55,15 +50,13 @@ function loadQuestion() {
             addBtnOption.classList.add('untrue')
             console.log("false")
           }
-      }) 
+      })
     }
 }
 
 getNextBtn.addEventListener('click', () => {
     // Incrémenter l'index de la question
     currentQuestionIndex++;
-    //console.log(currentQuestionIndex)
-
      // Vérifier s'il reste des questions
   if (currentQuestionIndex < quiz_adastories.questions.length) {
     // Afficher la question suivante
@@ -85,21 +78,3 @@ getReplayBtn.addEventListener('click', () => {
     getNextBtn.style.display = 'inline-block';
      loadQuestion()
 })
-
-const optionBtn = document.querySelector('.button-options')
-const optionBtnValue = optionBtn.innerText
-console.log(optionBtn)
-
-
-// for(const button of optionBtn){
-//   button.addEventListener('click', () => {
-//     for(const button of buttons){
-//       button.classList.remove()}
-    
-//     if (optionBtnValue == currentQuestion.correct_answer) {
-//       button.classList.add('correct')
-//     }
-//     else {
-//       button.classList.add('untrue')
-//     }
-// })}
