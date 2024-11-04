aa8fbc725934d7989b8de578458e770f63986c43
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2024-11-04 09:47:35 +0100

    update game.js

--- game.js
+++ game.js
@@ -1,10 +1,23 @@
-import {quiz_adastories} from "./questions.js";
+import { quiz_adastories } from './questions.js'; // Import des questions
 
-const getQuestion = document.querySelector('.question')
-// console.log("question" + " " + getQuestion.innerText)
-const getOptions = document.querySelector('.options')
-// console.log("options" + " " + getOptions.innerText)
-const getNextBtn = document.querySelector('#next-button')
+// DOM affichage
+const getQuestion = document.querySelector('.question');
+// console.log(getQuestion);
+const getOptions = document.querySelector('.options');
+// console.log(getOptions);
+const getNextBtn = document.querySelector('#next-button');
+// console.log(getNextBtn);
 
-let firstQuestion = quiz_adastories.questions[0]
-// console.log(firstQuestion)
+let firstQuestion = quiz_adastories.questions[0];
+// console.log(firstQuestion);
+
+// injection du texte dans le DOM :
+getQuestion.innerText = firstQuestion.text;
+
+for (const option of firstQuestion.options) {
+    const addBtnOption = document.createElement("button");
+    
+    getOptions.appendChild(addBtnOption);
+    addBtnOption.classList.add('button-options') ;
+    addBtnOption.innerText = option;   
+}
